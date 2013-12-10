3f41eb44c675054c97b010f948e91dc838e4be84
Adrienne <adrienne@dmgctrl.com>
2013-12-10 10:43:46 -0500

    Midterm 2: Question 2 - created static and instance area method

--- Problem Set 8/Question 2/draft_files/WindowShape.java
+++ Problem Set 8/Question 2/draft_files/WindowShape.java
@@ -6,64 +6,88 @@
 //(we would use this if we already had the WindowShape object and wanted its area)
 
 //Provide a static method area() that takes the width and height of the rectangular part
-//of the window and returens the area of the entire window - including the semi-circle
+//of the window and returns the area of the entire window - including the semi-circle
 //on top.
 //(we would use this method if we didn't have a WindowShape object and did not need
-//to create on)
+//to create one)
 
 //You are not given these method headers in this exercise. That is part of what you
 //need to figure out.
 
 //Use Math.PI
 
-// You can see the shape by looking at stained_glass_window_8b.png
-// which is in this project's folder.
+//Here is link to a window with that shape:
+//http://i.imgur.com/Wx1Klzw.png
 
 //For the draft, provide the instance method.
 
 /**
  * Models a window of a particular shape - namely a rectangle with a semi-circle on top
  */
-public class WindowShape
-{
+import java.lang.Math;
 
-    private double w;
-    private double h;
+public class WindowShape {
+
+    private double width;
+    private double height;
 
     /**
      * Constructor for objects of class WindowShape
      * @param width the width of the rectangle and diameter of the semi-circle
      * @param height the height of the rectangle alone
      */
-    public WindowShape(double width, double height)
-    {
-        w = width;
-        h = height;
+    public WindowShape(double width, double height) {
+        this.width = width;
+        this.height = height;
     }
 
     /**
      * Gets the width of this shape
      * @return the width
      */
-    public double getWidth()
-    {
-       return w;
+    public double getWidth() {
+       return width;
     }
 
     /**
      * Gets the height of this shape
      * @return the height
      */
-    public double getHeight()
-    {
-        return h;
+    public double getHeight() {
+        return height;
     }
 
+    /**
+     * Gets the area of the shape
+     * @return the area
+     */
+    public double area() {
+        double radius = .5 * width;
+        double boxHeight = height - radius;
+        double boxArea = height * width;
+        double circularArea = .5 * (Math.PI * Math.pow(radius, 2));
+        
+        return boxArea + circularArea;
+    }
+    
+    /**
+     * Gets the area of a window with a semi-circular top
+     * @return the area of the window
+     */
+    public static double area(double width, double height) {
+        double radius = .5 * width;
+        double boxHeight = height - radius;
+        double boxArea = height * width;
+        double circularArea = .5 * (Math.PI * Math.pow(radius, 2));
+        
+        return boxArea + circularArea;
+        
+    }
     // other methods for dealing with windows ...
 
     // TODO: Write an instance method to get the area of the shape
 
     // TODO: Write a static method that takes the width and height of the rectangle
-    // as parameters and returns the area of a rectangle wiht those dimensions and with a
+    // as parameters and returns the area of a rectangle with those dimensions and with a
     // semi-circle on top.
 }
