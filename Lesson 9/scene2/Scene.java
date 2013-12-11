73c4b1683ffaf4b88b23aa23d62796fb8bcfd5c8
Adrienne <adrienne@dmgctrl.com>
2013-12-11 14:17:24 -0500

    Lesson 9: scene 2 - combined three arrays into one interface array

--- Lesson 9/scene2/Scene.java
+++ Lesson 9/scene2/Scene.java
@@ -8,30 +8,18 @@ public class Scene
     {
         // TODO: Change to an ArrayList<Drawable>
 
-        ArrayList<Car> cars = new ArrayList<Car>();
-        cars.add(new Car(100, 300));
-        cars.add(new Car(250, 300));
-        cars.add(new Car(250, 350));
-
-        ArrayList<House> houses = new ArrayList<House>();
-        houses.add(new House(100, 150, 100, 100));
-        houses.add(new House(250, 100, 150, 150));
-
-        ArrayList<Dog> dogs = new ArrayList<Dog>();
-        dogs.add(new Dog(200, 100, "Fido.jpg"));
-        dogs.add(new Dog(100, 400, "Rex.jpg"));
-        dogs.add(new Dog(200, 400, "Lucky.jpg"));
+        ArrayList<Drawable> element = new ArrayList<Drawable>();
+            element.add(new Car(100, 300));
+            element.add(new Car(250, 300));
+            element.add(new Car(250, 350));
+            element.add(new House(100, 150, 100, 100));
+            element.add(new House(250, 100, 150, 150));
+            element.add(new Dog(200, 100, "Fido.jpg"));
+            element.add(new Dog(100, 400, "Rex.jpg"));
+            element.add(new Dog(200, 400, "Lucky.jpg"));
 
         // TODO: Change to a single loop
-        for (Car c : cars)
-        {
-            c.draw();
-        }
-        for (House h : houses)
-        {
-            h.draw();
-        }
-        for (Dog d : dogs)
+        for (Drawable d : element)
         {
             d.draw();
         }
