42b8997cb9f79a6028e55224c064365e57f198c8
Adrienne <adrienne@dmgctrl.com>
2013-12-11 16:38:14 -0500

    Lesson 9: measure 3 - added the Measurable interface to eliminate duplicate code when calculating an average

--- Lesson 9/measure3/Data.java
+++ Lesson 9/measure3/Data.java
@@ -1,26 +1,23 @@
-// BlueJ project: lesson9/measure3
-public class Data
-{
+public class Data {
     /**
        Computes the average measure of the given objects.
        @param objects an array of Measurable objects
        @return the average of the measures
     */
-    public static double average(Measurable[] objects)
-    {
+    public static double average(Measurable[] objects) {
         // TODO: Compute the average measure
         double sum = 0;
-        for (... obj : objects)
-        {
-            ...
+        int arraySize = objects.length;
+        double average = 0;
+        
+        for (Measurable obj : objects) {
+            sum += obj.getMeasure();
         }
-        if (objects.length > 0)
-        {
-            return sum / objects.length;
+        
+        if (arraySize > 0) {
+            average = sum / arraySize;
         }
-        else
-        {
-            return 0;
-        }
+        
+        return average;
     }
 }
